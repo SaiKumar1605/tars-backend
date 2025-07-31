63c73712767507bb56b777b5da07874707fc420a
SaiKumar1605 <ramagiri.saikumar09@gmail.com>
2025-07-31 11:53:00 +0530

    google API Auth changes for render

--- google_api/auth.py
+++ google_api/auth.py
@@ -10,13 +10,16 @@ SCOPES = [
 
 def get_google_creds():
     creds = None
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # if os.path.exists("token.json"):           #For local
+    #     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists("/etc/secrets/token.json"):
+        creds = Credentials.from_authorized_user_file("/etc/secrets/token.json", SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            # flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES) #For local
+            flow = InstalledAppFlow.from_client_secrets_file("/etc/secrets/credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
